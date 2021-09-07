cd387021702247ec9c3d75ce55fded8ab2d2ebd8
claudiosilva <claudioccdsilva@gmail.com>
2021-09-06 21:41:43 -0300

    Adicionado arquivos de projeto

---
+++ Drivers/BSP/VSM/inc/vsm_i2c.h
@@ -0,0 +1,19 @@
+/**
+ * @file vsm_i2c.h
+ * @author Cláudio Silva (claudioccdsilva@gmail.com)
+ * @brief  Cabeçalho do driver do i2c do projeto
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __VSM_I2C_H__
+#define __VSM_I2C_H__
+
+#include "stm32f4xx_hal_i2c.h"
+
+
+
+#endif /* __VSM_I2C_H__ */
